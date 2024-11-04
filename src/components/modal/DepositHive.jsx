f76954b5a39dae582ccd42e06156def8800842ab
Adesojisouljay <adesojiisrael@gmail.com>
2024-11-04 23:08:30 +0100

    fix simple styles

--- src/components/modal/DepositHive.jsx
+++ src/components/modal/DepositHive.jsx
@@ -112,7 +112,7 @@ export const DepositHiveModal = ({ isOpen, onClose, assets, user }) => {
           </span> */}
           <div className="wrap">
             <span>{selectedAsset?.depositAddress}</span>
-            <FaCopy onClick={handleCopyAddress} size={20}/>
+            <FaCopy className='copy-icon' onClick={handleCopyAddress} size={20}/>
           </div>
         </div>}
 
@@ -126,7 +126,7 @@ export const DepositHiveModal = ({ isOpen, onClose, assets, user }) => {
           <span>Deposit Memo:</span> 
           <div className="wrap">
             <span>{selectedAsset?.memo}</span>
-            <FaCopy onClick={handleCopyAddress} size={20}/>
+            <FaCopy className='copy-icon' onClick={handleCopyAddress} size={20}/>
           </div>
         </div>}
 
