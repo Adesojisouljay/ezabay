6f4ef79e5c49f9a539f7b11f961736fd8e281d76
kesolink <kingsleyumune14@gmail.com>
2024-11-10 15:37:22 +0100

    new commit on spendhbd blog

--- src/components/spend-hbd/SpendHbd.jsx
+++ src/components/spend-hbd/SpendHbd.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCommunityPosts } from "../../hive-client";
 import "./index.scss";
+import distrator from "../../assets/distrator-logo-no-bg.jpg"
+import { GoGift } from "react-icons/go";
+import { FaRetweet } from "react-icons/fa";
 
 export const SpendHbd = () => {
     const [posts, setPosts] = useState([]);
@@ -9,6 +12,7 @@ export const SpendHbd = () => {
     const [loading, setLoading] = useState(false);
     const [lastPost, setLastPost] = useState(null);
     const navigate = useNavigate();
+
 
     const fetchPosts = async () => {
         try {
@@ -58,13 +62,40 @@ export const SpendHbd = () => {
         fetchPosts();
     };
 
+    function timeAgo(dateString) {
+        const now = new Date();
+        const postDate = new Date(dateString);
+        const secondsAgo = Math.floor((now - postDate) / 1000);
+      
+        if (secondsAgo < 60) {
+          return `${secondsAgo} seconds ago`;
+        } else if (secondsAgo < 3600) {
+          const minutes = Math.floor(secondsAgo / 60);
+          return `${minutes} minutes ago`;
+        } else if (secondsAgo < 86400) {
+          const hours = Math.floor(secondsAgo / 3600);
+          return `${hours} hours ago`;
+        } else {
+          const days = Math.floor(secondsAgo / 86400);
+          return `${days} days ago`;
+        }
+      }
+    console.log(posts)
+    
+
     return (
         <div className="spend-hbd-container">
             <div className="spend-hbd-wrapper">
-                <h1>Trending Posts on SpendHbd</h1>
-                <div className="posts-grid">
+                <div className="cover-wrapper">
+                    <img src={distrator} alt="" />
+                </div>
+                <div className={`posts-grid ${loading && posts.length === 0 ? "loading" : ""}`}>
                     {loading && posts.length === 0 ? (
-                        <h2>Loading posts...</h2>
+                        <div className="loader-wrapper">
+                            <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+
+                            <h2>Loading posts...</h2>
+                        </div>
                     ) : posts.length ? (
                         posts.map((post, index) => (
                             <div
@@ -72,24 +103,39 @@ export const SpendHbd = () => {
                                 className="post-card"
                                 onClick={() => handlePostClick(post.author, post.permlink)}
                             >
+                                <div className="info-wrap">
+                                    <span>{post.author}</span>
+                                     <span className="time wrap">{timeAgo(post.created)}</span>
+                                </div>
                                 {post.body && (
                                     <>
                                         {extractImageUrl(post.body) && (
+                                            <div className="wrap-img">
                                             <img 
                                                 src={extractImageUrl(post.body)} 
                                                 alt={post.title} 
                                                 className="post-image" 
                                             />
+                                            </div>
                                         )}
                                     </>
                                 )}
+                                <div className="wrap-content">
                                 <h2 className="post-title">{post.title.substring(0, 30)}...</h2>
-                                <p>Created: {post.created}</p>
                                 <p className="post-body">
                                     {cleanPostBody(post.body).length > 200 
                                         ? `${cleanPostBody(post.body).substring(0, 200)}...` 
                                         : cleanPostBody(post.body)}
                                 </p>
+                                <div className="info-wrap-down">
+                                    <div className="reward-wrap">
+                                    <GoGift /> <span>{post.payout.toFixed(2)}</span> 
+                                    </div>
+                                    <div className="reward-wrap">
+                                    <FaRetweet /> <span>{post.reblogs}</span> 
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                         ))
                     ) : (
