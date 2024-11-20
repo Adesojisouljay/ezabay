7f26966d29892fe0e4bb8c1a959b9d841fb9086a
Adesojisouljay <adesojiisrael@gmail.com>
2024-11-20 04:44:15 +0100

    fix login error messages

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,10 +4,7 @@ export const registerUser = async (userData) => {
   try {
     const response = await api.post('/auth/register', userData);
 
-    console.log('Response from server:', response);
-
     if (response?.data?.success) {
-      console.log('User registered successfully');
       return response?.data;
     } else {
       console.error('Failed to register user:', response?.data.message);
@@ -28,20 +25,12 @@ export const registerUser = async (userData) => {
 export const loginUser = async (userData) => {
   try {
     const response = await api.post('/auth/login', userData);
-    console.log(response)
-    if (response?.data.success) {
-      console.log('User logged in successfully');
 
       localStorage.setItem('token', response?.data.token);
       return response; 
-    } else {
-      console.error('Failed to login:', response?.message);
-      console.log(response)
-
-      return null;
-    }
+ 
   } catch (error) {
     console.error('Error logging in:', error);
-    throw error.message;
+    return error
   }
 };
